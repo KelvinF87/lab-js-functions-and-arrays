c128a22f19d16b10753d277836b107b8bd34c3a5
KelvinFamilia <kelvinjosefamilia@gmail.com>
2025-01-15 15:28:20 +0000

    lab Resuelto

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,21 +1,48 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
+function maxOfTwoNumbers( num1, num2) {
+    let maxNum = Math.max(num1, num2);
+    return maxNum;
+}
+console.log(maxOfTwoNumbers( 20,21))
 
 
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(listWords) {
+    let longest="";
+    if (listWords.length === 0){
+        return null;
+    }else{
 
+        for(let word of listWords ){
+            if(word.length > longest.length){
+                longest= word
+            } 
+        }
+        
+        return longest;
+    }
+}
 
+console.log(findLongestWord(words))
 
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    let total=0;
+    for (let num  of numbers) {
+       total+=num
+            
+    }
+
+    return total
+}
+
+sumNumbers(numbers)
 
 
 
@@ -23,12 +50,32 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers2) {
+    let total=0;
+    if(numbers2.length!==0){
 
+        let conteo = numbers2.length
+        for (let num  of numbers2) {
+            total+=num
+            
+        }
+        return total/conteo
+    }else{
+        return 0
+    }
 
-
+}
 
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(words2,search) {
+    if (words2.includes(search)){
+        return true;
+    }else if(words2.length==0){
+        return null
+    }
+    else{
+        return false;
+    }
+}
